5e71ada71ced598c3c19863641906017e6b30688
kasi Durgarao <kasidurgarao183@gmail.com>
2025-02-13 11:59:34 +0530

    Rebuild

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,17 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: "https://beautiful-platypus-ab63fb.netlify.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
