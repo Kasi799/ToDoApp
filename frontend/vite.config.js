ae342e49f8fdee6834a3edbf121809c853b6ef0f
kasi Durgarao <kasidurgarao183@gmail.com>
2025-02-13 12:50:03 +0530

    Updated API calls for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "https://todoapp-emlp.onrender.com", 
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    proxy: process.env.NODE_ENV === "development"
+      ? {
+          "/api": {
+            target: "http://localhost:5000", 
+            changeOrigin: true,
+            secure: false,
+          },
+        }
+      : undefined,
   },
 });
